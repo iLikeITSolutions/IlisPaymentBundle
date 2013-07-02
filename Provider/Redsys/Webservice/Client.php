012b4053992bf322d0620f1dc6e514c8a9ede1ec
Carlo D'Ambrosio <carlo@ql4b.net>
2013-07-02 10:08:12 +0200

    typo fix

--- Provider/Redsys/Webservice/Client.php
+++ Provider/Redsys/Webservice/Client.php
@@ -9,7 +9,7 @@
 
 namespace Ilis\Bundle\PaymentBundle\Provider\Redsys\Webservice;
 
-class ClientClient extends \Zend\Soap\Client
+class Client extends \Zend\Soap\Client
 {
     // Environments
     const ENV_INTEGRATION						= 'integration';
