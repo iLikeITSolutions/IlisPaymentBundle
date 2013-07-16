4e28f37290b33cb782456259c7196ab7acbde120
Carlo D'Ambrosio <carlo@ql4b.net>
2013-07-16 14:18:34 +0200

    Added transaction.update event

---
+++ Event/TransactionUpdatedEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * (c) iLIKE IT Solutions
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ilis\Bundle\PaymentBundle\Event;
+
+class TransactionUpdatedEvent extends TransactionEventAbstract
+{
+
+}
